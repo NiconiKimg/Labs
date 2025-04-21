178807f708479906ad782d6a1fb26ff6facad02d
NiconiKimg <pedemontenicolas2004@gmail.com>
2025-04-21 08:17:17 -0300

    a
    a

---
+++ UnidadesPractica/Unidad2/Capitulo01/Lab01.ParaPensar/NumeroAMes/Program.cs
@@ -0,0 +1,15 @@
+﻿
+string[] meses = new string[] {"enero", "febrero", "marzo", "abril", "mayo", "junio",
+                                "julio", "agosto", "septiembre", "octubre", "noviembre",
+                                "diciembre"};
+
+Console.Write("Ingrese un mes a buscar: ");
+string mesBusq = Console.ReadLine();
+
+for (int i = 0; i < meses.Length; i++)
+{
+    if(mesBusq.ToLower() == meses[i])
+    {
+        Console.Write($"{mesBusq} es el mes número {i + 1}");
+    }
+}
